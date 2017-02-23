66a267455f6a062fed11ead88a7bcd7863424fd0
sax5 <sax5@L322629>
2017-02-23 09:03:03 -0500

    Addition of SourceTable object

---
+++ EpiInfo.Database.EIWeb/dbo/Tables/SourceTables.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[SourceTables] (
+    [SourceTableName] VARCHAR (200)    NOT NULL,
+    [FormId]          UNIQUEIDENTIFIER NOT NULL,
+    [SourceTableXml]  XML              NOT NULL,
+    CONSTRAINT [PK_SourceTables] PRIMARY KEY CLUSTERED ([SourceTableName] ASC, [FormId] ASC)
+);
+
